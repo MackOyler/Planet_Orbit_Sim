ac15046523fbf2ba483a5e405bfe869ffbfe9369
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-15 18:36:48 -0500

    sun added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
 
 class Planet:
     AU = 149.6e6 * 1000
@@ -28,19 +29,33 @@ class Planet:
         
         self.x_vel = 0
         self.y_vel = 0
+        
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x, y), self.radius)
 
 def main():
     run = True
     clock = pygame.time.Clock()
     
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+    
+    planets = [sun]
+    
     while run:
         clock.tick(60)
         #WIN.fill(WHITE)
-        #pygame.display.update()
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                
+        for planet in planets:
+            planet.draw(WIN)        
+          
+        pygame.display.update()  
                 
     pygame.quit()            
     
